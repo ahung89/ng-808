caa48362906ca171f9fca0a1a07238ed31992a6a
Leigh Caplan <texel1@gmail.com>
2015-08-12 20:48:45 -0700

    Turn these into instance methods.

--- app/js/sequence.js
+++ app/js/sequence.js
@@ -1,22 +1,8 @@
 var Sequence = function ( attributes ) {
   attributes = attributes || {};
 
-  // Because writing out an array literal with 16
-  // elements is boring (and error-prone).
-  var defaultLevels = function () {
-    return _.map( _.range(16), function () {
-      return 0;
-    });
-  };
-
-  var mapLevels = function ( levels ) {
-    return _.map( levels, function ( level ) {
-      return new SequenceEvent({level: level});
-    });
-  };
-
   this.numSteps    = attributes.steps || 16;
-  this.events      = mapLevels(attributes.levels || defaultLevels());
+  this.events      = this.mapLevels(attributes.levels || this.defaultLevels());
   this.currentStep = 0;
 };
 
@@ -27,5 +13,19 @@ _.extend( Sequence.prototype, {
     this.currentStep = (this.currentStep + 1) % this.numSteps;
 
     return event;
+  },
+
+  // Because writing out an array literal with 16
+  // elements is boring (and error-prone).
+  defaultLevels: function () {
+    return _.map( _.range(16), function () {
+      return 0;
+    });
+  },
+
+  mapLevels: function ( levels ) {
+    return _.map( levels, function ( level ) {
+      return new SequenceEvent({level: level});
+    });
   }
 });
