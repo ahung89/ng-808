a1c8c1981b4b5aa0dec317780ac738d6fde10120
Leigh Caplan <texel1@gmail.com>
2015-08-12 20:20:55 -0700

    Add a level toggle a current level readout per part.

--- app/js/part.js
+++ app/js/part.js
@@ -1,4 +1,5 @@
 var Part = function ( attributes ) {
   this.name = attributes.name;
   this.sequence = new Sequence();
+  this.currentEvent = this.sequence.next();
 };
--- app/js/sm_808.js
+++ app/js/sm_808.js
@@ -2,4 +2,12 @@ var sm808 = angular.module("sm808", [])
 
 .controller("MainController", function ( $scope ) {
   $scope.drumMachine = new DrumMachine();
+
+  $scope.toggleLevel = function ( event ) {
+    event.level = ( event.level + 64 );
+
+    if ( event.level > 128 ) {
+      event.level = 0;
+    }
+  };
 });
